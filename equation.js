3f34cb1ed4ae645250168660715695893890e5e0
justine <justinemangaliman@gmail.com>
2023-08-10 16:25:19 -0600

    fixed adding operator next to another operator

--- equation.js
+++ equation.js
@@ -9,6 +9,19 @@ const divideSymbol = '/';
 const timesSymbol = 'x';
 const decimalSymbol = '.';
 
+function operatorInLastIndex() {
+  if (
+    equation[equation.length - 1] === timesSymbol || 
+    equation[equation.length - 1] === divideSymbol || 
+    equation[equation.length - 1] === addSymbol || 
+    equation[equation.length - 1] === minusSymbol
+  ) {
+      return true;
+  } else {
+    return false;
+  }
+}
+
 // creates a new number
 function addToEquation(item) {
 
@@ -16,8 +29,17 @@ function addToEquation(item) {
     if (item != timesSymbol && item != minusSymbol && item != addSymbol && item != divideSymbol) {
       equation = item;
     }
-  } else {
-    if (equation.length < maxEquationLength) {
+  } else if (equation.length < maxEquationLength) {
+    if (operatorInLastIndex()) {
+      if (
+        item != timesSymbol &&
+        item != divideSymbol &&
+        item != addSymbol &&
+        item != minusSymbol
+      ) {
+        equation += item;
+      }
+    } else {
       equation += item;
     }
   }
@@ -47,15 +69,9 @@ function checkForDecimal() {
 }
 
 function solveEquation() {
-  if (
-    equation[equation.length - 1] === timesSymbol || 
-    equation[equation.length - 1] === divideSymbol || 
-    equation[equation.length - 1] === addSymbol || 
-    equation[equation.length - 1] === minusSymbol
-    ) {
+  if (operatorInLastIndex()) {
         alert("Invalid format used.");
-      }
-  else {
+  } else {
     // equation = `${eval(equation)}`; nice try you're not allowed to do this
     document.querySelector('.message').innerHTML = equation;
   }
