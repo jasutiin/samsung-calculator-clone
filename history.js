30193ab1b2ccb9d9cc84babb0df4ae8e9a698b63
justine <justinemangaliman@gmail.com>
2023-08-12 18:34:41 -0600

    added basic history button functionality

--- history.js
+++ history.js
@@ -1,1 +1,19 @@
 'use strict';
+
+let historyButtonStatus = false;
+let historyWindow = document.querySelector('.history-div');
+
+const historyButton = document.querySelector('#history-button');
+historyButton.addEventListener(
+  'click',
+  (e) => {
+    if (historyButtonStatus) { // currently on, want to turn off
+      historyWindow.classList.add('hidden');
+      historyButtonStatus = false;
+    } else { // currently off, want to turn on
+      historyWindow.classList.remove('hidden');
+      historyButtonStatus = true;
+    }
+  },
+  false
+);
